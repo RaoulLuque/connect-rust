c4ca719c0289c83e1abd3902a824df662bb45fcf
Raoul Luqué <raoulsluque@gmail.com>
2023-09-08 19:42:59 +0200

    Initial commit and transferring framework

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use connect_rust_logging::connect_four_logging::Logger;
+use connect_rust_players::players::{Player,human};
+use std::{time::Instant, thread::current};
+use connect_rust_gamestate_helpers;
+
+pub mod setup;
+
+/// Plays the connect four game and asks which players/engines should play against which.
+/// If human players are playing, gamestates are shown in console directly otherwise they are visible in logs
+fn main() {
+    // Printing explanation of game
+    setup::print_introduction();
+
+    // Choosing who to play as/against (choosing players)
+    let (mut player_blue, mut player_red) =  setup::read_in_players();
+
+    // Setup of variables for game
+    let mut current_gamestate: u32 = 0;
+    let mut turn_number: i32 = 0;
+    let mut log = Logger::new();
+
+
+    // Running the game
+    while gamestate_helpers::is_won(current_gamestate) == 0 || gamestate_helpers::is_over(current_gamestate) {
+        // Increment turn number
+        turn_number += 1;
+
+        // Timing how long it took to calculate turn
+        let timer = Instant::now();
+
+        // Chooses the next move based on the current player who's turn it is and the engine chosen
+        let mut next_move: u32 =
+            {if turn_number % 2 == 0 {
+                player_blue.make_move(current_gamestate)
+            } else {
+                player_blue.make_move(current_gamestate)
+            }
+            };
+
+        // Taking time
+        let elapsed = timer.elapsed();
+        let elapsed = elapsed.as_millis();
+
+        // Checking whether move was valid
+        if !connect_rust_gamestate_helpers::is_allowed_move(current_gamestate, next_move, turn_number) {
+            // Move is invalid, logged and game is stopped
+            log.log_invalid_turn(turn_number, current_gamestate, next_move).expect("Logging should be possible");
+            break;
+
+        } else {
+            // Move is valid and is logged
+            current_gamestate = current_gamestate | next_move;
+
+        }
+    }
+
+}
---
+++ src/setup.rs
@@ -0,0 +1,42 @@
+use std::io;
+use connect_rust_players::players::{Player,human};
+
+pub fn print_introduction() {
+    // Introduction
+    println!("Welcome to connect-rust, a connect-four implementation in rust.");
+    println!("The options are to play oneself against oneself or against one of the bots or having the bots play against each other.");
+    println!("So far the bots are referring to: None");
+}
+
+pub fn read_in_players() -> (Player, Player) {
+    println!("Please choose the gamemode you want to play by writing XvY. X and Y standing for the players. Playing yourself is signaled with an H for human and the different bots are abbreviated above");
+    println!("E.g.: If you want to play against the bruteforce bot write 'HvB'.");
+    println!("You can also write 'BvB' or 'HvH' for the bruteforce bots playing against each other and you playing against another human.");
+    println!("Also note that with HvB the human would be starting (playing as blue) and with BvP the other way around.");
+    let mut input = String::new();
+
+    // Infinite loop checking the input for a valid input
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(error) => panic!("Problem reading in input: {:?}", error),
+        }
+
+        // Matching the input to the 
+        match input.trim() {
+            "HvH" => {return (Player::Human(human::Engine::new()), Player::Human(human::Engine::new()))},
+            _ => {println!("Not a valid input! Please try again:");},
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reading_in_players() {
+       let (player_blue, player_red) = read_in_players();
+
+    }
+}
