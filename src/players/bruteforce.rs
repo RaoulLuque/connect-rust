827cff612a0402240d2bfe9f39c8dc4d109625a0
RaoulLuque <raoulsluque@gmail.com>
2023-09-14 13:06:02 +0200

    Implemented make_move

--- src/players/bruteforce.rs
+++ src/players/bruteforce.rs
@@ -7,8 +7,13 @@ use connect_rust_graphs::graph::Graph;
 
 pub struct Engine {
     color: PlayerColor, 
+    // Gamestates graph where gamestates have labels with their evaluation as i32.
+    // "" is the initial evaluation
+    // Evaluation +i32::max-x stands for the current player winning in x turns 
+    // Evaluation -i32::max+x stands for the opposing player winning in x turns
     gamestate_graph: Graph<u32>,
 }
+
 
 impl Engine {
     /// Constructor for new engine constructing empty gamestate graph
@@ -18,11 +23,29 @@ impl Engine {
         res.initialize_graph();
 
         res
+
+        
     }
 
     /// Returns the best possible move accounting to the gamestate graph calculated at initialization
     pub fn make_move(&self, gamestate: u32) -> u32 {
-        0
+        let mut best_succesor: u32 = 0;
+        for successor in self.gamestate_graph.out_neighbours(&gamestate) {
+            if best_succesor == 0 {
+                best_succesor = *successor;
+            }
+            if let (Ok(s), Ok(b)) = (self.gamestate_graph.get_label(successor).unwrap().parse(),
+                                  self.gamestate_graph.get_label(&best_succesor).unwrap().parse()) {
+                if s > b {
+                    best_succesor = *successor;
+                } else {
+                    panic!("Gamestate label should be an u32!");
+                }
+            }
+        }
+        best_succesor
+
+        
     }
 
     /// Initializes the gamestate graph with all possible gamestates
@@ -44,7 +67,7 @@ impl Engine {
                 if !self.gamestate_graph.is_vertex_in_graph(&next_gamestate) {
                     unvisited.push_back(next_gamestate)
                 }
-                self.gamestate_graph.add_vertex_with_label(next_gamestate, "-1");
+                self.gamestate_graph.add_vertex_with_label(next_gamestate, "");
                 self.gamestate_graph.add_edge(current_gamestate, next_gamestate).expect("Gamestates should be in the gamestate graph");
             }          
         }
