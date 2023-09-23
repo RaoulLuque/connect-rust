efc9e34f87f8490bc4b29e6d18434319f3edfaa0
RaoulLuque <raoulsluque@gmail.com>
2023-09-23 14:13:25 +0200

    Changed read_in_players to seperate and added log

--- src/setup.rs
+++ src/setup.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, time::Instant};
 use crate::players::{Player, human, bruteforce};
 use crate::gamestate_helpers::PlayerColor;
 
@@ -9,30 +9,58 @@ pub fn print_introduction() {
     println!("So far the bots are referring to: None");
 }
 
-pub fn read_in_players() -> (Player, Player) {
+pub fn read_in_players() -> (Player, Player, u128, u128) {
     println!("Please choose the gamemode you want to play by writing XvY. X and Y standing for the players. Playing yourself is signaled with an H for human and the different bots are abbreviated above");
     println!("E.g.: If you want to play against the bruteforce bot write 'HvB'.");
     println!("You can also write 'BvB' or 'HvH' for the bruteforce bots playing against each other and you playing against another human.");
     println!("Also note that with HvB the human would be starting (playing as blue) and with BvP the other way around.");
-    let mut input = String::new();
 
     // Infinite loop checking the input for a valid input
     loop {
-        input = String::new();
+        let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(_) => (),
             Err(error) => panic!("Problem reading in input: {:?}", error),
         }
 
-        // Matching the input to the 
-        match input.trim() {
-            "HvH" => {return (Player::Human(human::Engine::new(PlayerColor::Blue)), Player::Human(human::Engine::new(PlayerColor::Red)))},
-            "BvH" => {return (Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Blue)), Player::Human(human::Engine::new(PlayerColor::Red)))},
-            "HvB" => {return (Player::Human(human::Engine::new(PlayerColor::Blue)), Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Red)))},
-            "BvB" => {return (Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Blue)), Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Red)))},
-            _ => {println!("Not a valid input! Please try again:");},
+        // Players
+        let player_one_blue: Player;
+        let player_two_red: Player;
+
+        // Elapsed
+        let mut elapsed_time_one_blue: u128 = 0;
+        let mut elapsed_time_two_red: u128 = 0;
+
+
+        input = input.trim().to_owned();
+
+        // Checking the inputs for the engine abbreviations
+        if input.starts_with("H") {
+            player_one_blue = Player::Human(human::Engine::new(PlayerColor::Blue));
+        } else if input.starts_with("B") {
+            let timer = Instant::now();
+            player_one_blue = Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Blue));
+            let elapsed = timer.elapsed();
+            elapsed_time_one_blue = elapsed.as_millis();
+        } else {
+            println!("Not a valid input for first player! Please try again:");
+            continue;
         }
-    }
+
+        if input.ends_with("H") {
+            player_two_red = Player::Human(human::Engine::new(PlayerColor::Red));
+        } else if input.ends_with("B") {
+            let timer = Instant::now();
+            player_two_red = Player::Bruteforce(bruteforce::Engine::new(PlayerColor::Red));
+            let elapsed = timer.elapsed();
+            elapsed_time_two_red = elapsed.as_millis();
+        } else {
+            println!("Not a valid input for first player! Please try again:");
+            continue;
+        }
+        
+        return (player_one_blue, player_two_red, elapsed_time_one_blue, elapsed_time_two_red)
+    } 
 }
 
 pub fn declare_winner(winner: &Option<PlayerColor>, turn_number: usize) {
