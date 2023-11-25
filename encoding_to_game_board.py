fbf283305589f2c05a9e9147e70d4f7adcf7c419
Raoul Luqué <raoulsluque@gmail.com>
2023-11-25 18:30:21 +0100

    Added encoding_to_game_board.py python script

---
+++ encoding_to_game_board.py
@@ -0,0 +1,30 @@
+# To not track changes of this file in your git: git update-index --assume-unchanged game_board_to_encoding.py
+# To revert the above: git update-index --no-assume-unchanged game_board_to_encoding.py
+
+# Enter the gamestate of the board to be decoded here
+encoded_gamestate = 0
+
+if __name__ == '__main__':
+    res = ""
+    row_counter = 1
+    column_counter = 1
+
+    for i in range(6):
+        for j in range(7):
+            if 1 & encoded_gamestate == 1:
+                res += "B "
+
+            elif 2 & encoded_gamestate == 2:
+                res += "R "
+
+            else:
+                res += "O "
+
+            encoded_gamestate //= 4
+        res += "\n"
+
+    print("Decoding of given gamestate is:")
+    print(res)
+
+            
+
