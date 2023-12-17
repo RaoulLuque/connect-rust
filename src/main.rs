ffd6af95e76003c1d649c6e613c25d2155ae6c1d
Raoul Luqué <raoulsluque@gmail.com>
2023-12-17 17:33:35 +0100

    Changed port for #27

--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,8 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(start_page))
         .route("/", post(accept_move));
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // run our app with hyper, listening globally on port 8080
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
