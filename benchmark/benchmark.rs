64b2dea2018b2acc95459fed030acd3af5b98e34
Raoul Luqué <raoulsluque@gmail.com>
2023-12-28 21:15:21 +0100

    Finished Benchmarking of engines fixes #35

--- benchmark/benchmark.rs
+++ benchmark/benchmark.rs
@@ -1,6 +1,7 @@
 use connectrustlibrary::gamestate_helpers::turn_series_of_columns_to_encoded_gamestate;
 use connectrustlibrary::players::Player;
 use std::fs::read_to_string;
+use std::env::current_dir;
 
 fn main() {
     let player = Player::Bruteforce;
@@ -24,13 +25,15 @@ fn run_all_benchmarks(player: Player) -> () {
 // Progress of game: 3 - lategame; 2 - midgame; 1 - earlygame
 fn run_specific_benchmark(progress_of_game: u32, difficulty_of_set: u32, engine: &Player) -> () {
     let mut total_number_of_examples: u32 = 0;
-    let mut total_computation_time: u32 = 0;
-    let mut total_number_of_explored_nodes: u32 = 0;
+    let mut total_computation_time: u32 = 1;
+    let mut total_number_of_explored_nodes: u32 = 1;
     let mut total_number_of_failed_examples: u32 = 0;
 
     let dataset_filename: String = format!("Test_L{}_R{}", progress_of_game, difficulty_of_set);
+    let dataset_path: String = format!("benchmark/benchmarks/{}",dataset_filename.clone());
 
-    for line in read_to_string(dataset_filename.clone()).unwrap().lines() {
+    for line in read_to_string(&dataset_path)
+    .expect(&format!("File: {} should exist", &dataset_path)).lines() {
         let mut line = line.split_whitespace();
         let current_gamestate = turn_series_of_columns_to_encoded_gamestate(
             line.next().expect("Line should have gamestate"),
@@ -50,7 +53,7 @@ fn run_specific_benchmark(progress_of_game: u32, difficulty_of_set: u32, engine:
         total_number_of_explored_nodes += number_of_explored_nodes;
         total_computation_time += computation_time;
 
-        if (actual_evaluation != expected_evaluation) {
+        if actual_evaluation != expected_evaluation {
             println!("On the {}th example following missevaluation was made: The evaluation was supposed to be: {}. The engine suggested the evaluation: {}", 
             total_number_of_examples, expected_evaluation, actual_evaluation);
             total_number_of_failed_examples += 1;
@@ -63,6 +66,7 @@ fn run_specific_benchmark(progress_of_game: u32, difficulty_of_set: u32, engine:
         "In total {} examples were calculated",
         total_number_of_examples
     );
+    println!("{} of such examples failed", total_number_of_failed_examples);
 
     println!("---");
     println!("The mean time is: {}, the mean number of positions: {}, and the number of positions per second: {}", 
