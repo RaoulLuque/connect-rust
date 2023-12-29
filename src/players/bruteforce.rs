e53f3f65ee4dfe9c30e83009bc61345292e51fb8
Raoul Luqué <raoulsluque@gmail.com>
2023-12-29 19:59:27 +0100

    Implemented iterative deepening with null window search

--- src/players/bruteforce.rs
+++ src/players/bruteforce.rs
@@ -15,7 +15,7 @@ impl Engine {
         let time = Instant::now();
         let mut number_of_visited_nodes: u32 = 0;
 
-        let (min, max) = match WEAK_SOLVE {
+        let (mut min, mut max) = match WEAK_SOLVE {
             false => (
                 -(WIDTH * HEIGHT - (number_of_turns_played(current_gamestate) as i8)) / 2,
                 (WIDTH * HEIGHT - (number_of_turns_played(current_gamestate) as i8)) / 2,
@@ -23,19 +23,29 @@ impl Engine {
             true => (-1, 1),
         };
 
-        let evaluation: i8 = negamax(
-            current_gamestate,
-            min,
-            max,
-            whos_turn_is_it_gamestate(current_gamestate),
-            &mut number_of_visited_nodes,
-        );
+        while min < max {
+            let mut med = min + (max - min) / 2;
 
-        (
-            0,
-            evaluation,
-            number_of_visited_nodes,
-            time.elapsed().as_micros(),
-        )
+            if med <= 0 && min / 2 < med {
+                med = min / 2
+            } else if med >= 0 && max / 2 > med {
+                med = max / 2
+            }
+
+            let evaluation = negamax(
+                current_gamestate,
+                med,
+                med + 1,
+                whos_turn_is_it_gamestate(current_gamestate),
+                &mut number_of_visited_nodes,
+            );
+
+            if evaluation <= med {
+                max = evaluation;
+            } else {
+                min = evaluation;
+            }
+        }
+        (0, min, number_of_visited_nodes, time.elapsed().as_micros())
     }
 }
