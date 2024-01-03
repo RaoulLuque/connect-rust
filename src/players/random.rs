9fc3871ffe113b6851896e2fff14f6a81fc8b27b
Raoul Luqué <raoulsluque@gmail.com>
2024-01-03 18:46:13 +0100

    Random actually plays randomly now fixes #45

--- src/players/random.rs
+++ src/players/random.rs
@@ -1,10 +1,13 @@
 use crate::helpers::moves::possible_next_gamestates;
 pub struct Engine;
 
+use rand::seq::SliceRandom;
+
 impl Engine {
     pub fn make_move(current_gamestate: u128) -> (u128, i8, u32, u128) {
+        let possible_move: Vec<u128> = possible_next_gamestates(current_gamestate).collect();
         (
-            possible_next_gamestates(current_gamestate).last().unwrap(),
+            *possible_move.choose(&mut rand::thread_rng()).unwrap(),
             0,
             0,
             0,
