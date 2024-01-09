3041fc5e6d3bb4ce59cee655b061a2e0a04f4b21
Raoul Luqué <raoulsluque@gmail.com>
2024-01-09 21:58:27 +0100

    Changed favicon

--- src/response_handling/html_template.rs
+++ src/response_handling/html_template.rs
@@ -349,7 +349,7 @@ pub const START_PAGE_TEMPLATE: &'static str = r#"
         <title>Connect-Rust</title>
         <meta name="description" content="A connect-four game implemented in Rust">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-        <link rel="icon" href="https://ludii.games/images/games/thumbnails/thumb-Connect%20Four-b.png">
+        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/c/c2/Connect_4_game_logo.png">
     </head>
     <div class="center">
         <h1>
