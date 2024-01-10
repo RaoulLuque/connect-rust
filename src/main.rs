6001cd62e7044929489814d72ed39ae65b118d82
Raoul Luqué <raoulsluque@gmail.com>
2024-01-10 19:07:25 +0100

    Added handling of 404 (trying to access page that doesnt exist)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use connectrustlibrary::response_handling::{incoming, outgoing};
 
 use axum::{
+    handler::HandlerWithoutStateExt,
+    http::StatusCode,
     routing::{get, post},
     Router,
 };
 
 #[tokio::main]
 async fn main() {
+    let service = handle_404.into_service();
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(outgoing::start_page))
-        .route("/", post(incoming::accept_move));
+        .route("/", post(incoming::accept_move))
+        .fallback_service(service);
     // run our app with hyper, listening globally on port 8080
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_404() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Nothing to see here")
 }
